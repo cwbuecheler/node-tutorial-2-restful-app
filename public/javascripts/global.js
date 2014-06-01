601c86afc2a2c1d17008b3e2e5db81ad151a0bed
Christopher Buecheler <cwbuecheler@gmail.com>
2014-06-01 07:11:20 -0400

    fixed a missing close anchor in global.js

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -35,7 +35,7 @@ function populateTable() {
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
             tableContent += '<td>' + this.email + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
