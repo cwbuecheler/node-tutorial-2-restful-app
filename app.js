ce9d87e9cb282d0c7b052b99615f286e15578473
Christopher Buecheler <cwbuecheler@gmail.com>
2014-10-17 11:31:56 -0400

    fix for server-favicon

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
