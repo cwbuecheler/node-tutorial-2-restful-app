27195c0022e56083a9b1d9f0ec6919df463d9ab9
Christopher Buecheler <cwbuecheler@gmail.com>
2018-04-04 11:52:47 -0400

    Updating for latest versions of Node, Express, and MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/*
- * GET userlist.
- */
+/* GET userlist. */
 router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    collection.find({},{},function(e,docs){
-        res.json(docs);
-    });
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.find({},{},function(e,docs){
+    res.json(docs);
+  });
 });
 
-/*
- * POST to adduser.
- */
+/* POST to adduser. */
 router.post('/adduser', function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    collection.insert(req.body, function(err, result){
-        res.send(
-            (err === null) ? { msg: '' } : { msg: err }
-        );
-    });
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
 });
 
-/*
- * DELETE to deleteuser.
- */
+/* DELETE to deleteuser. */
 router.delete('/deleteuser/:id', function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    var userToDelete = req.params.id;
-    collection.remove({ '_id' : userToDelete }, function(err) {
-        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
-    });
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+  });
 });
 
 module.exports = router;
